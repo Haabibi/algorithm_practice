d68493d0e598d8d39fe5a89e07c56675a162e738
Abigail Haakyung Lee <haabibi623@gmail.com>
2019-07-18 15:45:32 +0900

    remove_nth_node done

--- LeetCode/linked_lists/remove_nth_node.py
+++ LeetCode/linked_lists/remove_nth_node.py
@@ -17,6 +17,65 @@ Given n will always be valid.
 Follow up:
 Could you do this in one pass?
 
-
-
 """
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        if n is bigger than the length of linked list, 
+        remove the first item 
+
+        >>> s = Solution()
+        >>> appendLinkedList(s.removeNthFromEnd(createLinkedList([1]), 1))
+        []
+        >>> appendLinkedList(s.removeNthFromEnd(createLinkedList([1, 2, 3, 4, 5]), 2))
+        [1, 2, 3, 5]
+        >>> appendLinkedList(s.removeNthFromEnd(createLinkedList([1, 2, 3, 4]), 5))
+        [2, 3, 4]
+        
+        Time Complexity: O(N1 + N2) 
+        - N1: traversing linked list to get length 
+        - N2: traversing again until len-nth item 
+        Space Complexity: O(1)
+        - inplace 
+        """
+
+        # find the length of a given linked list
+        cur_head = head
+        total_len = 0
+        while cur_head:
+            total_len += 1
+            cur_head = cur_head.next
+
+        # traverse from the beginning and find the nth elem from the end
+        from_front_idx = total_len - n - 1
+
+        cur_head = head
+        while from_front_idx > 0 and cur_head:
+            from_front_idx -= 1
+            cur_head = cur_head.next
+
+        if from_front_idx < 0:
+            return cur_head.next
+        else:
+            cur_head.next = cur_head.next.next
+            cur_head = cur_head.next
+
+        return head
+
+
+if __name__ == "__main__":
+    import doctest
+    from helper_functions import createLinkedList, appendLinkedList
+
+    doctest.testmod()
